e27ac57c6502e6e0c9126f42e9365b3f187ea5fa
Lawrence <lawrence.goldstien@madetech.com>
2019-09-02 16:44:28 +0100

    adds frontend asset server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('frontend/build'));
+
 const port = process.env.PORT || 3000;
 app.listen(port, console.log(`Listening on ${port}`));
